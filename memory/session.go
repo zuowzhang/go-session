e0b1018d39b55aef4a051f40a25b88ab7fcfcdc4
张斌 <zbin@meizu.com>
2017-09-13 12:06:23 +0800

    1. 完成session manager以及memory实现

---
+++ memory/session.go
@@ -0,0 +1,111 @@
+package memory
+
+import (
+	"time"
+	"session"
+	"sync"
+	"container/list"
+)
+
+var memoryProvider = &MemoryProvider{
+	sessions:make(map[string]*list.Element, 0),
+	sessionList:list.New(),
+}
+
+func init() {
+	session.RegisterProvider("memory", memoryProvider)
+}
+
+type MemoryStore struct {
+	id               string
+	lastTimeAccessed time.Time
+	values           map[interface{}]interface{}
+}
+
+func (store *MemoryStore)Set(key, value interface{}) error {
+	store.values[key] = value
+	memoryProvider.update(store.id)
+	return nil
+}
+
+func (store *MemoryStore)Get(key interface{}) interface{} {
+	value, ok := store.values[key]
+	if ok {
+		return value
+	}
+	return nil
+}
+
+func (store *MemoryStore)Delete(key interface{}) error {
+	delete(store.values, key)
+	memoryProvider.update(store.id)
+	return nil
+}
+
+func (store *MemoryStore)Id() string {
+	return store.id
+}
+
+type MemoryProvider struct {
+	lock        sync.RWMutex
+	sessions    map[string]*list.Element
+	sessionList *list.List
+}
+
+func (provider *MemoryProvider)Read(sid string) (session.Session, error) {
+	ok := false
+	provider.lock.RLock()
+	element, ok := provider.sessions[sid]
+	provider.lock.RUnlock()
+	if !ok {
+		provider.lock.Lock()
+		defer provider.lock.Unlock()
+		element = provider.sessionList.PushFront(&MemoryStore{
+			id:sid,
+			lastTimeAccessed:time.Now(),
+			values:make(map[interface{}]interface{}),
+		})
+		provider.sessions[sid] = element
+
+	}
+	return element.Value.(*MemoryStore), nil
+}
+
+func (provider *MemoryProvider)Remove(sid string) error {
+	provider.lock.Lock()
+	defer provider.lock.Unlock()
+	element, ok := provider.sessions[sid]
+	if ok {
+		delete(provider.sessions, sid)
+		provider.sessionList.Remove(element)
+	}
+	return nil
+}
+
+func (provider *MemoryProvider)Gc(maxLifeTime int64) {
+	provider.lock.Lock()
+	defer provider.lock.Unlock()
+	for {
+		element := provider.sessionList.Back()
+		if element == nil {
+			break
+		}
+		if element.Value.(*MemoryStore).lastTimeAccessed + maxLifeTime < time.Now().Unix() {
+			provider.sessionList.Remove(element)
+			delete(provider.sessions, element.Value.(*MemoryStore).id)
+		} else {
+			break
+		}
+	}
+}
+
+func (provider *MemoryProvider)update(sid string) {
+	provider.lock.Lock()
+	defer provider.lock.Unlock()
+	element, ok := provider.sessions[sid]
+	if ok {
+		element.Value.(*MemoryStore).lastTimeAccessed = time.Now()
+		provider.sessionList.MoveToFront(element)
+	}
+	return nil
+}
---
+++ sessionManager.go
@@ -0,0 +1,123 @@
+package session
+
+import (
+	"sync"
+	"errors"
+	"io"
+	"crypto/rand"
+	"encoding/base64"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+type Provider interface {
+	Read(sid string) (Session, error)
+	Remove(sid string) error
+	Gc(maxLifeTime int64)
+}
+
+type Session interface {
+	Set(interface{}, interface{}) error
+	Get(interface{}) interface{}
+	Delete(interface{}) error
+	Id() string
+}
+
+type SessionMgr struct {
+	cookieName  string
+	lock        sync.Mutex
+	maxLifeTime int64
+	provider    Provider
+}
+
+var providers = make(map[string]Provider)
+
+func RegisterProvider(providerName string, provider Provider) {
+	if provider == nil {
+		panic("provider is nil")
+	}
+	if _, ok := providers[providerName]; ok {
+		panic("duplicated registe session provider")
+	}
+	providers[providerName] = provider
+}
+
+func NewSessionMgr(providerName, cookieName string, maxLifeTime int64) (*SessionMgr, error) {
+	provider, ok := providers[providerName]
+	if !ok {
+		return nil, errors.New("unknown session provider")
+	}
+	mgr := &SessionMgr{
+		cookieName:cookieName,
+		maxLifeTime:maxLifeTime,
+		provider:provider,
+	}
+	go mgr.Gc()
+	return mgr, nil
+}
+
+func sessionId() string {
+	b := make([]byte, 32)
+	if _, err := io.ReadFull(rand.Reader, b); err != nil {
+		return ""
+	}
+	return base64.URLEncoding.EncodeToString(b)
+}
+
+func (mgr *SessionMgr)SessionStart(w http.ResponseWriter, r *http.Request) Session {
+	mgr.lock.Lock()
+	defer mgr.lock.Unlock()
+	cookie, err := r.Cookie(mgr.cookieName)
+	if err != nil || cookie.Value == "" {
+		sessionId := sessionId()
+		if sessionId != "" {
+			session, err := mgr.provider.Read(sessionId)
+			if err == nil {
+				cookie := http.Cookie{
+					Name:mgr.cookieName,
+					Value:sessionId,
+					Path:"/",
+					HttpOnly:true,
+					MaxAge:int(mgr.maxLifeTime),
+				}
+				http.SetCookie(w, &cookie)
+				return session
+			}
+		}
+	} else {
+		sessionId := url.PathEscape(cookie.Value)
+		session, err := mgr.provider.Read(sessionId)
+		if err == nil {
+			return session
+		}
+	}
+	return nil
+}
+
+func (mgr *SessionMgr)SessionStop(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie(mgr.cookieName)
+	if err != nil || cookie.Value == "" {
+		return
+	}
+	mgr.lock.Lock()
+	defer mgr.lock.Unlock()
+	mgr.provider.Remove(cookie.Value)
+	cookie = &http.Cookie{
+		Name:mgr.cookieName,
+		Path:"/",
+		HttpOnly:true,
+		Expires:time.Now(),
+		MaxAge:-1,
+	}
+	http.SetCookie(w, cookie)
+}
+
+func (mgr *SessionMgr)Gc() {
+	mgr.lock.Lock()
+	defer mgr.lock.Unlock()
+	mgr.provider.Gc(mgr.maxLifeTime)
+	time.AfterFunc(time.Duration(mgr.maxLifeTime), func() {
+		mgr.Gc()
+	})
+}
